26a10e838c4ae09cb6a1d16cac2a4264e924c408
Taras Tarasiuk <taras_tarasiuk@epam.com>
2023-04-27 18:30:01 +0300

    Permission handling & fragments init

--- app/src/main/java/com/tarastarasiuk/myapplication/MainActivity.java
+++ app/src/main/java/com/tarastarasiuk/myapplication/MainActivity.java
@@ -1,16 +1,30 @@
 package com.tarastarasiuk.myapplication;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+@RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
 public class MainActivity extends AppCompatActivity {
 
+    private static final String REQUIRED_MEDIA_PERMISSION = Manifest.permission.READ_MEDIA_VIDEO;
+    private boolean isStorageVideoPermitted = false;
     private BottomNavigationView bottomNavigationView;
 
     @Override
@@ -20,6 +34,12 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.bottomNavView);
         bottomNavigationView.setOnItemSelectedListener(getOnItemSelectedListener());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        permissionsRequest();
     }
 
     @NonNull
@@ -37,6 +57,51 @@ public class MainActivity extends AppCompatActivity {
             }
             return false;
         };
+    }
+
+
+    private void permissionsRequest() {
+        if (!isStorageVideoPermitted) {
+            requestPermissionStorage();
+        } else {
+            Toast.makeText(this, "Video storage permission granted.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void requestPermissionStorage() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, REQUIRED_MEDIA_PERMISSION) == PackageManager.PERMISSION_GRANTED) {
+            isStorageVideoPermitted = true;
+        } else {
+            requestPermissionLauncherStorageImages.launch(REQUIRED_MEDIA_PERMISSION);
+        }
+    }
+
+    private final ActivityResultLauncher<String> requestPermissionLauncherStorageImages =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(),
+                    isGranted -> {
+                        if (isGranted) {
+                            isStorageVideoPermitted = true;
+                        } else {
+                            isStorageVideoPermitted = false;
+                            sendToSettingDialog();
+                        }
+                    });
+
+    private void sendToSettingDialog() {
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Alert for permission")
+                .setMessage("Go to setting and enable media permissions to use this app")
+                .setPositiveButton("Settings", (dialogInterface, i) -> {
+                    Intent settingsIntent = new Intent();
+                    settingsIntent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    settingsIntent.setData(uri);
+                    startActivity(settingsIntent);
+                    dialogInterface.dismiss();
+                })
+                .setNegativeButton("Exit", (dialogInterface, i) -> finish())
+                .setCancelable(false)
+                .show();
     }
 
 }
